a3330694603b5ec41685058524f8fb17eca555de
Kyle Peacock <kylpeacock@gmail.com>
2022-01-07 16:01:07 -0800

    automated install with install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
-dfx canister install --mode reinstall --argument '(record {send_whitelist=vec{}; minting_account="051b05839339f89053454a4b9865ea0452a4bffe2b1cd41f4982bad10c1e637c"; transaction_window = null; max_message_size_bytes = null; archive _options = null; initial_values = vec {record{"bdc4ee05d42cd0669786899f256c8fd7217fa71177bd1fa7b9534f568680a938"; record {e8s=100_000_000_000}}};})' ledger
+dfx stop
+dfx start --background --clean
+
+dfx identity new minter
+dfx identity use minter
+export MINT_ACC=$(dfx ledger account-id)
+
+dfx identity use default
+export LEDGER_ACC=$(dfx ledger account-id)
+
+# Use private api for install
+rm src/ledger/ledger.did
+cp src/ledger/ledger.private.did src/ledger/ledger.did
+
+dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
+
+# Replace with public api
+rm src/ledger/ledger.did
+cp src/ledger/ledger.public.did src/ledger/ledger.did
+
+dfx deploy invoice
