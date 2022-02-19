6e91087c1687eb93c2c7c97b8dbc2917512db8b8
Kyle Peacock <kylpeacock@gmail.com>
2022-02-18 17:05:41 -0800

    wip

---
+++ examples/motoko-seller-client/declarations/seller/seller.did.d.ts
@@ -0,0 +1,65 @@
+import type { Principal } from '@dfinity/principal';
+export type AccountIdentifier = { 'principal' : Principal } |
+  { 'blob' : Array<number> } |
+  { 'text' : string };
+export interface CreateInvoiceErr {
+  'kind' : { 'InvalidDetails' : null } |
+    { 'InvalidAmount' : null } |
+    { 'InvalidDestination' : null } |
+    { 'InvalidToken' : null } |
+    { 'Other' : null },
+  'message' : [] | [string],
+}
+export type CreateInvoiceResult = { 'ok' : CreateInvoiceSuccess } |
+  { 'err' : CreateInvoiceErr };
+export interface CreateInvoiceSuccess { 'invoice' : Invoice }
+export interface Details { 'meta' : Array<number>, 'description' : string }
+export interface Invoice {
+  'id' : bigint,
+  'permissions' : [] | [Permissions],
+  'creator' : Principal,
+  'destination' : AccountIdentifier,
+  'token' : TokenVerbose,
+  'refundedAtTime' : [] | [bigint],
+  'paid' : boolean,
+  'refunded' : boolean,
+  'verifiedAtTime' : [] | [bigint],
+  'amountPaid' : bigint,
+  'expiration' : bigint,
+  'refundAccount' : [] | [AccountIdentifier],
+  'details' : [] | [Details],
+  'amount' : bigint,
+}
+export interface Permissions {
+  'canGet' : Array<Principal>,
+  'canVerify' : Array<Principal>,
+}
+export interface TokenVerbose {
+  'decimals' : bigint,
+  'meta' : [] | [{ 'Issuer' : string }],
+  'symbol' : string,
+}
+export interface VerifyInvoiceErr {
+  'kind' : { 'InvalidAccount' : null } |
+    { 'TransferError' : null } |
+    { 'NotFound' : null } |
+    { 'NotAuthorized' : null } |
+    { 'InvalidToken' : null } |
+    { 'InvalidInvoiceId' : null } |
+    { 'Other' : null } |
+    { 'NotYetPaid' : null } |
+    { 'Expired' : null },
+  'message' : [] | [string],
+}
+export type VerifyInvoiceResult = { 'ok' : VerifyInvoiceSuccess } |
+  { 'err' : VerifyInvoiceErr };
+export type VerifyInvoiceSuccess = { 'Paid' : { 'invoice' : Invoice } } |
+  { 'AlreadyVerified' : { 'invoice' : Invoice } };
+export interface _SERVICE {
+  'check_license_status' : () => Promise<boolean>,
+  'create_invoice' : () => Promise<CreateInvoiceResult>,
+  'get_invoice' : () => Promise<[] | [Invoice]>,
+  'remaining_cycles' : () => Promise<bigint>,
+  'reset_license' : () => Promise<undefined>,
+  'verify_invoice' : () => Promise<VerifyInvoiceResult>,
+}
